d8f066ac2e9d2beb357dd5563041ee04bdcc5217
artem <artem.rokhin@jetbrains.com>
2017-10-26 12:53:30 +0300

    PR-2

--- 123.js
+++ 123.js
@@ -24,6 +24,8 @@ asfsdfsdfdf
 sfsdfsf
 
 фывфыв
+
+фывфывфыв
 asfasdf98uy98987fd asfasdf98uy98987fdasfasdf98uy98987fd asfasdf98uy98987fdasfasdf98uy98987fd asfasdf98uy98987fdasfasdf98uy98987fd asfasdf98uy98987fdasfasdf98uy98987fd asfasdf98uy98987fdasfasdf98uy98987fd asfasdf98uy98987fd
 
 zczczxcz

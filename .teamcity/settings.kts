2acde6221512e4c48cda767274a389bb15727945
Artem Rokhin <artem.rokhin@jetbrains.com>
2022-08-23 18:18:10 +0200

    imul str

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,38 +1,43 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
 
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
 
 version = "2022.04"
 
 project {
 
-    buildType(Build)
-    buildType(Build2)
+    buildType(Router)
+    buildType(DockerRouter)
 }
 
-object Build : BuildType({
-    name = "Build"
+object Router : BuildType({
+    name = "Router"
+
+    vcs {
+        root(DslContext.settingsRoot, "+:Router",)
+    }
+    steps {
+        script {
+            name = "show file structure"
+            scriptContent = """
+                bash pwd
+                ls ./
+            """.trimIndent()
+
+        }
+
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
+
+object DockerRouter : BuildType({
+    name = "Docker"
 
     vcs {
         root(DslContext.settingsRoot)
@@ -44,15 +49,10 @@ object Build : BuildType({
     }
 })
 
-object Build2 : BuildType({
-    name = "Build2"
-
+object RouterDepoy : BuildType ({
+    name = "Router Deploy"
     vcs {
         root(DslContext.settingsRoot)
-    }
 
-    triggers {
-        vcs {
-        }
     }
 })
